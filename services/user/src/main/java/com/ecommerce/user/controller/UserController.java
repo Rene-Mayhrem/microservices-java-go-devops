22827b4f4fc1d317c361157482c99adbe9b7d922
mayhrem <renecruz.0711@gmail.com>
2025-10-02 20:46:42 -0600

    Adding REST endpoints

---
+++ services/user/src/main/java/com/ecommerce/user/controller/UserController.java
@@ -0,0 +1,59 @@
+package com.ecommerce.user.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.user.dto.UserRequest;
+import com.ecommerce.user.dto.UserResponse;
+import com.ecommerce.user.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService service;
+
+    //? CREATE NEW USER
+    @PostMapping
+    public ResponseEntity<UserResponse> createUser (@RequestBody UserRequest request){
+        return ResponseEntity.ok(service.createUser(request));
+    }
+
+    //? GET ALL USERS
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getAllUsers () {
+        return ResponseEntity.ok(service.getAllUsers());
+    }
+
+    //? GET USER BY ID
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUserById (@PathVariable Long id) {
+        return ResponseEntity.ok(service.getUserById(id));
+    }
+
+    //? DELETE USER
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser (@PathVariable Long id) {
+        service.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    //? UPDATE USER
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> updateUser (@PathVariable Long id,
+                                                    @RequestBody UserRequest request) {
+        return ResponseEntity.ok(service.updateUser(request, id));
+    }
+}
--- services/user/src/main/java/com/ecommerce/user/service/UserService.java
+++ services/user/src/main/java/com/ecommerce/user/service/UserService.java
@@ -1,9 +1,14 @@
 package com.ecommerce.user.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 import com.ecommerce.user.dto.UserRequest;
 import com.ecommerce.user.dto.UserResponse;
+import com.ecommerce.user.model.User;
+import com.ecommerce.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,11 +16,64 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserService {
     
-    private final repository;
+    private final UserRepository repository;
 
     //? Create new user
     public UserResponse createUser (UserRequest request) {
+        User user = User.builder()
+            .username(request.getUsername())
+            .email(request.getEmail())
+            .password(request.getPassword())
+            .firstName(request.getFirstName())
+            .lastName(request.getLastName())
+            .build();
+        User savedUser = repository.save(user);
+        return mapToResponse(savedUser);
+    }
 
+    //? Get all users
+    public List<UserResponse> getAllUsers () {
+        return repository.findAll()
+            .stream()
+            .map(this::mapToResponse)
+            .collect(Collectors.toList());
+    }
+
+    //? Get user by id
+    public UserResponse getUserById (Long id) {
+        User user = repository.findById(id)
+            .orElseThrow(() -> new RuntimeException("User not found"));
+        return mapToResponse(user);
+    }
+
+    //? Update user
+    public UserResponse updateUser (UserRequest request, Long id) {
+        User user = repository.findById(id)
+            .orElseThrow(() -> new RuntimeException("User not found"));
+        
+        user.setUsername(request.getUsername());
+        user.setEmail(request.getEmail());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setPassword(request.getPassword());
+        User udpatedUser = repository.save(user); //? update user
+        return mapToResponse(udpatedUser);
+    }
+
+    //? Delete user
+    public void deleteUser (Long id) {
+        repository.deleteById(id);
+    }
+
+    //? Mapper
+    private UserResponse mapToResponse(User user) {
+        return UserResponse.builder()
+            .id(user.getId())
+            .username(user.getUsername())
+            .email(user.getEmail())
+            .firstName(user.getFirstName())
+            .lastName(user.getLastName())
+            .build();
     }
 
 }
